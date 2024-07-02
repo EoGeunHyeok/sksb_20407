7890fc18037f1d022f1bad1882fb2207aac972f4
EoGeunHyeok <eghsamo@naver.com>
2024-07-02 14:04:50 +0900

    member domain

--- src/main/java/com/example/sksb/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/sksb/domain/member/controller/ApiV1MemberController.java
@@ -14,17 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class ApiV1MemberController {
-
     private final MemberService memberService;
 
     @Getter
-    public static class LoginResponsebody {
+    public static class LoginResponseBody {
         private String username;
 
-        public LoginResponsebody(String username) {
+        public  LoginResponseBody(String username) {
             this.username = username;
         }
     }
@@ -34,19 +33,19 @@ public class ApiV1MemberController {
     public static class LoginRequestBody {
         @NotBlank
         private String username;
-
         @NotBlank
         private String password;
     }
 
     @PostMapping("/login")
-    public LoginResponsebody login(@Valid @RequestBody LoginRequestBody body) {
+    public LoginResponseBody login(@Valid @RequestBody LoginRequestBody body) {
         Member member = memberService.findByUsername(body.getUsername())
                 .orElseThrow(() -> new GlobalException("400-1", "해당 유저가 존재하지 않습니다."));
 
-        if (memberService.passwordMatches(member, body.getPassword()) == false) {
+        if ( memberService.passwordMatches(member, body.getPassword()) == false ) {
             throw new GlobalException("400-2", "비밀번호가 일치하지 않습니다.");
         }
-        return new LoginResponsebody(member.getUsername());
+
+        return new LoginResponseBody(member.getUsername());
     }
 }
