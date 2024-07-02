0ea0391ad35d0c7ab2b9aaf0891b3139f499a627
EoGeunHyeok <eghsamo@naver.com>
2024-07-02 14:30:34 +0900

    make token

--- src/main/java/com/example/sksb/domain/member/service/MemberService.java
+++ src/main/java/com/example/sksb/domain/member/service/MemberService.java
@@ -2,6 +2,9 @@ package com.example.sksb.domain.member.service;
 
 import com.example.sksb.domain.member.entity.Member;
 import com.example.sksb.domain.member.repository.MemberRepository;
+import com.example.sksb.global.rsData.RsData;
+import com.example.sksb.global.excepctions.GlobalException;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -33,4 +36,32 @@ public class MemberService {
        memberRepository.save(member);
     }
 
+    @Getter
+    public static class AuthAndMakeTokensResponseBody {
+        private String accessToken;
+        private String refreshToken;
+
+        public AuthAndMakeTokensResponseBody(String accessToken, String refreshToken) {
+            this.accessToken = accessToken;
+            this.refreshToken = refreshToken;
+        }
+    }
+
+    @Transactional
+    public RsData<AuthAndMakeTokensResponseBody> authAndMakeTokens(String username, String password) {
+        Member member = findByUsername(username)
+                .orElseThrow(() -> new GlobalException("400-1", "해당 유저가 존재하지 않습니다."));
+
+        if (!passwordMatches(member, password))
+            throw new GlobalException("400-2", "비밀번호가 일치하지 않습니다.");
+
+        String refreshToken = "refreshToken"; // 진짜 토큰은 아니고 임의적으로 만들어서 한것.
+        String accessToken = "accessToken"; // 진짜 토큰은 아니고 임의적으로 만들어서 한것.
+
+        return RsData.of(
+                "200-1",
+                "로그인 성공",
+                new AuthAndMakeTokensResponseBody(accessToken, refreshToken)
+        );
+    }
 }
