69078cd4b36b0d35cc18edd7ddb09ca605bf63b0
EoGeunHyeok <eghsamo@naver.com>
2024-07-03 18:11:52 +0900

    login 수정

--- src/main/java/com/example/sksb/global/security/ApiSecurityConfig.java
+++ src/main/java/com/example/sksb/global/security/ApiSecurityConfig.java
@@ -1,6 +1,4 @@
 package com.example.sksb.global.security;
-
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +8,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableMethodSecurity // method에서 보안을 추가할 수 있는것.
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .securityMatcher("/api/**")
                 .authorizeRequests(
@@ -26,15 +24,14 @@ public class ApiSecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-
                 .csrf(
                         csrf -> csrf
                                 .disable()
                 )
-                .cors( //도메인에서 요청을 하거나 제한하는데 사용
-                        cors -> cors.configure(http)
+                .cors(cors -> cors
+                        .configure(http)
                 )
-                .sessionManagement( // 매니지먼트 세션관리 비활성화
+                .sessionManagement(
                         sessionManagement -> sessionManagement
                                 .disable()
                 )
